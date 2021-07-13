e11a4dde8f966555bc55258a6df7748973e955a8
unknown <bradley.powell18@gmail.com>
2021-07-13 11:10:43 -0500

    Beginning of Iterations

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,13 +8,24 @@ import os
 import csv
 
 # Store the file path associated with the file -- include 'r' pre-filepath 
-csvPath = R"C:\Users\bradl\Desktop\Git\nwBootCamp\python-challenge\PyBank\Resources\budget_data.csv"
+csvFile = R"C:\Users\bradl\Desktop\Git\nwBootCamp\python-challenge\PyBank\Resources\budget_data.csv"
 
 # Read CSV file 
 
-with open(csvPath, 'r') as filePath:
-    csvReader = filePath.read()
+with open(csvFile, 'r') as budgetFile:
+    csvReader = budgetFile.read()
     print (csvReader)
 
+    # Iterate through the rows in the file 
+    for row in csvReader:
+        totalMonths.append(row[0])
+        totalProfit.append(int(row[1]))
 
+    # Iterate through profits to get monthly change in profits
+    for i in range (len(totalProfit)-1):
+        monthlyProfitChange.append(totalProfit[i+1])
+
+        
+
+# Find the total number of months in the dataset 
 
