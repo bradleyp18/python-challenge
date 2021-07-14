d1f176708dd8607e8adef0cb8dd4cca934670250
unknown <bradley.powell18@gmail.com>
2021-07-14 18:31:53 -0500

    Big Chunk of Code (Most of the functionality)

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -17,8 +17,44 @@ with open(pollData) as pollFile:
 
     # Find the total number of votes that were casted (this doesn't include the header)
     next(csvReader)
-    numberVotes = list(csvReader)
-    rowCount = len(numberVotes)
+    candData = list(csvReader)
+    rowCount = len(candData)
+
+    # Create a list of Candidates 
+    candidateList = []
+    candMark = []
+
+    # Run through the file 
+    for c in range (0, rowCount):
+
+        #Create candidate variable that tracks for loops through the 2nd index (3rd column)
+        candidate = candData[c][2]
+        candMark.append(candidate)
+
+        # Conditional to keep track of candidate list
+        if candidate not in candidateList:
+            candidateList.append(candidate)
+    candidateCount = len(candidateList)
+
+    # Find the vote count for each candidate and their respective %
+    votes = []
+    percentage = []
+
+    for v in range(0, candidateCount):
+        candidateName = candidateList[v]
+        votes.append(candData.count(candidateName))
+        votePct = votes[v] / rowCount
+        percentage.append(votePct)
+    winningCandidate = votes.index(max(votes))
+
+    bars = 30 * '-'
+    print ('Election Results')
+    print (bars)
+    print (f"Total Votes: {rowCount: ,}")
+    print (bars)
+    
+    
+
 
 
 
